7dda87856ecb4d4a8a18bcf1e6d7019f7ef2a5b0
chenyucong <2206424401@qq.com>
2019-11-05 10:59:46 +0800

    【opt】购物车光度弹窗修改

--- src/api/order.js
+++ src/api/order.js
@@ -3,15 +3,27 @@ import http from '@/utils/request.js';
 const api = {
   //根据订单状态获取订单列表
   getOrderList({queryState, isFreshCache = false, page = 1, size = 10} = {}) {
-    return http.postByNoLoading(`/order/GetOrderList?QueryState=${queryState}&Size=${size}&Page=${page}&isFreshCache=${isFreshCache}`);
+    let url = `/order/GetOrderList?QueryState=${queryState}&Size=${size}&Page=${page}&isFreshCache=${isFreshCache}`
+    if(page > 1){
+      return http.post(url)
+    }
+    return http.postByNoLoading(url)
   },
   //根据关键字查询订单/获取全部订单
   searchOrderList({queryString = '', page = 1, size = 10} = {}){
-    return http.postByNoLoading(`/Order/QueryOrderList?Size=${size}&Page=${page}&queryString=${queryString}`)
+    let url = `/Order/QueryOrderList?Size=${size}&Page=${page}&queryString=${queryString}`
+    if(page > 1){
+      return http.post(url)
+    }
+    return http.postByNoLoading(url)
   },
   //查询订单详情
   getOrderDetail(orderId){
     return http.post(`/order/GetOrderDetail?orderId=${orderId}`)
+  },
+  //取消订单
+  cancelOrder(orderId){
+    return http.post(`/order/CancelOrder?orderId=${orderId}`)
   },
 };
 
