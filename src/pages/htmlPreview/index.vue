2258e865b54519c5b04c79b1c140f2231f404bb5
chenyucong <2206424401@qq.com>
2019-11-06 19:38:25 +0800

    【opt】新增webview展示页面

---
+++ src/pages/htmlPreview/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <web-view :src="url" @error="error" @load="succ"></web-view>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      url: ''
+    };
+  },
+  onLoad(options){
+    if(options['path']){
+      this.url = 'https://m.kede.com/'+decodeURIComponent(options.path)
+    }
+  },
+  methods:{
+    error(e){
+      console.error('网页加载失败',e)
+    },
+    succ(e){
+      console.log('网页加载成功',e)
+    }
+  }
+};
+</script>
+
+<style>
+</style>
