28630ca1f3b688cc2297682b44dc20bbf447352e
chenyucong <2206424401@qq.com>
2019-12-24 10:11:25 +0800

    【other】readme文件更新

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,15 +1,17 @@
 module.exports = {
-    // apiurl: 'http://192.168.117.39:8004',
-    // salePlatformId: '65322024-9A29-4393-BAD5-CDA5B8034ADE'
-    // salePlatformId: '5758035A-B02A-46F2-A9BB-B1D318DABBF9'
-  
-    // apiurl: 'http://114.80.166.24:18050',
-  // apiurl: 'http://114.80.166.131:18050',
-    // apiurl: 'http://192.168.117.58:8060',
-    apiurl: 'http://192.168.117.39:8004',
+  // apiurl: 'http://192.168.117.39:8004',
+  // salePlatformId: '65322024-9A29-4393-BAD5-CDA5B8034ADE'
+  // salePlatformId: '5758035A-B02A-46F2-A9BB-B1D318DABBF9'
 
-  salePlatformId: '63710AD6-A235-458F-BFCF-1BFEE63CAB35'
-  
-    // apiurl: 'https://mpapi.kede.com/',
-    // salePlatformId: '65322024-9A29-4393-BAD5-CDA5B8034ADE'
-  }
+  // apiurl: 'http://114.80.166.24:18050',
+  // apiurl: 'http://114.80.166.131:18050',
+
+  apiurl: 'http://114.80.166.30:8002/',
+  salePlatformId: '6B1F905D-C1B6-489D-B547-F12DAAB98208',
+
+  // apiurl: 'http://192.168.117.39:8004',
+  // salePlatformId: '63710AD6-A235-458F-BFCF-1BFEE63CAB35'
+
+  // apiurl: 'https://mpapi.kede.com/',
+  // salePlatformId: '65322024-9A29-4393-BAD5-CDA5B8034ADE'
+}
